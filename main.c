01e1f0681bfa3b9a1c42b158f0e1af6d68b63db8
Andrey Pahomov <pahomov.and@gmail.com>
2021-11-11 19:24:56 +0200

    Add loop thread
    Signed-off-by: Andrey Pahomov <pahomov.and@gmail.com>

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "RPMSG.h"
 
 RPMSG_t rpmsg;
 char buff[BUFF_SIZE];
+pthread_t thread;
+bool isRun;
 
-int main() {
-    printf("Hello, World!\n");
-
+void *loop(void*p) {
     RPMSG_Start(&rpmsg, "/dev/RPMSG0");
 
-    bool isRun = true;
+    isRun = true;
     while (isRun) {
         if (RPMSG_GetLine(&rpmsg, &buff[0]) > 0) {
             printf("-> %s\n", &buff[0]);
         }
     }
+}
 
+int main() {
+    int status_addr;
+    int status;
+    printf("Hello, World!\n");
+
+
+    if (pthread_create(&thread, NULL, loop, (void*) NULL) != 0) {
+        printf("Error");
+        return -1;
+    }
+
+    status = pthread_join(thread, (void**)&status_addr);
+    if (status != 0) {
+        printf("Error: can't join thread, status = %d\n", status);
+        exit(-1);
+    }
+
+    getchar();
     return 0;
 }
